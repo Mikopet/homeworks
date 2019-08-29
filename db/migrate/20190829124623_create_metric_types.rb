1bf6d7b527757717f10dd55063bcd4c6b0df1c4c
Peter Mikola <mikopet@gmail.com>
2019-08-29 14:36:29 +0200

    generate fake data

---
+++ db/migrate/20190829124623_create_metric_types.rb
@@ -0,0 +1,7 @@
+class CreateMetricTypes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :metric_types do |t|
+      t.string :name
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Metric.destroy_all
+MetricType.destroy_all
+
+MetricType.create!([{
+  id: 1,
+  name: 'SETUP start'
+}, {
+  id: 2,
+  name: 'SETUP end'
+},{
+  id: 3,
+  name: 'STEP start'
+},{
+  id: 4,
+  name: 'STEP end'
+},{
+  id: 5,
+  name: 'PASSWORD regeneration'
+}])
+
+1000.times do
+  Metric.create!(
+    timestamp: Faker::Time.between(from: 1.year.ago, to: 1.day.ago),
+    customer_id: rand(21..100),
+    admin_id: rand(1..20),
+    metric_type_id: rand(1..5)
+  )
+end
