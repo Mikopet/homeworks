8911712074af8f0420622038f44462d178261387
Peter Mikola <mikopet@gmail.com>
2019-08-30 08:01:31 +0200

    the main task chart

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -8,4 +8,23 @@ class Metric < ApplicationRecord
   }
 
   scope :setup, -> { where(metric_type_id: [:setup_start, :setup_end]) }
+
+  def setup_pair
+    scope = setup_start? ? :setup_end : :setup_start
+    Metric.send(scope).find_by(customer_id: customer_id)
+  end
+
+  def in_a_month?
+    timestamp.beginning_of_month == setup_pair.timestamp.beginning_of_month
+  end
+
+  def duration
+    timestamp - setup_pair.timestamp
+  end
+
+  class << self
+    def setups
+      metric_type_ids.first(2).to_h
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   get 'setup_start_counts_by_month', to: 'chart#setup_start_counts_by_month'
   get 'setup_counts_cumulated_by_month', to: 'chart#setup_counts_cumulated_by_month'
+
+  get '/task', to: 'chart#task'
+
+  get 'completed_setup_by_month', to: 'chart#completed_setup_by_month'
+  get 'average_setup_duration_by_month', to: 'chart#average_setup_duration_by_month'
 end
