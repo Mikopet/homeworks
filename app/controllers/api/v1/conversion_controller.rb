42495e802de0f96b5ecbfd30d8fe134e036c2e57
Peter Mikola <mikopet@gmail.com>
2020-07-13 12:56:28 +0200

    some more features about creating a conversion job

--- app/controllers/api/v1/conversion_controller.rb
+++ app/controllers/api/v1/conversion_controller.rb
@@ -1,22 +1,41 @@
 module Api
   module V1
     class ConversionController < ApplicationController
+      VALID_FORMATS = %w[step iges stl obj]
+
       def create
+        @response = Hash.new
+        @response['errors'] = []
+
+        # Thats not a good pattern. This is business logic in controller, but the damn jbuilder didnt want to work
+        # So thats a TODO to refact
         if permitted_params[:data].nil?
-          render json: { message: 'There was no data at all' }, status: :bad_request
-        elsif permitted_params[:data]
-          render json: { message: 'There was no valid data file' }, status: :unsupported_media_type
+          @response['status'] ||= :bad_request
+          @response['errors'] << { message: 'There was no data at all' }
+        elsif VALID_FORMATS.exclude? permitted_params[:target]
+          @response['status'] ||= :unsupported_media_type
+          @response['errors'] << { message: "There was no valid target format (#{VALID_FORMATS})" }
+        elsif permitted_params[:data].split(';').first != 'data:document/shapr'
+          @response['status'] ||= :unsupported_media_type
+          @response['errors'] << { message: 'There was no valid data file' }
+        else
+          @response['status'] = :ok
+          @response['message'] = "The conversion is queued"
+          @response['uuid'] = SecureRandom.uuid
+          # enqueue the job
         end
+
+        render json: @response, status: @response['status']
       end
 
-      def show(uuid)
+      def show
 
       end
 
       private
 
       def permitted_params
-        params.permit(:name, :data)
+        params.permit(:name, :data, :target)
       end
     end
   end
