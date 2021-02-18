1d9a6af12ec42308d1f5e71d634523fd8a32f960
Peter Mikola <mikopet@gmail.com>
2021-02-18 18:32:12 +0100

    extend sport model #bvtech

--- bvtech/app/models/sport.rb
+++ bvtech/app/models/sport.rb
@@ -2,7 +2,18 @@ class Sport < ApplicationRecord
   validates :external_id, :name, presence: true
   validates :external_id, uniqueness: true
   validates :name, uniqueness: { scope: :external_id, message: 'This name is already exists with this external_id' }
+  validates :name, uniqueness: { scope: :active, message: 'This name is already active in other record' }, if: :active
   validates :active, :inclusion => { :in => [true, false] }
 
   scope :active, -> { where(active: true) }
+
+  def activate
+    self.class.where.not(id: id).where(name: name).map(&:inactivate)
+    update(active: true)
+  end
+
+  def inactivate
+    update(active: false)
+  end
 end
+
--- bvtech/spec/models/sport_spec.rb
+++ bvtech/spec/models/sport_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Sport, type: :model do
     end
 
     context 'with non-unique name in scope of external_id' do
-      let(:sport1) { create :sport, name: 'Brockian Ultra-Cricket', external_id: 42 }
+      let(:sport1) { create :sport, name: 'Brockian Ultra-Cricket', external_id: 42, active: false }
       let(:sport2) { build :sport, name: 'Brockian Ultra-Cricket', external_id: 42 }
 
       it 'raises error' do
@@ -32,8 +32,20 @@ RSpec.describe Sport, type: :model do
       end
     end
 
-    context 'with non-unique name' do
+    context 'with non-unique name both active' do
       let(:sport1) { create :sport, name: 'Brockian Ultra-Cricket', external_id: 42 }
+      let(:sport2) { build :sport, name: 'Brockian Ultra-Cricket', external_id: 43 }
+
+      it 'raises error' do
+        sport1
+        expect { sport2.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+
+  context 'create valid model' do
+    context 'with non-unique name with inactive old' do
+      let(:sport1) { create :sport, name: 'Brockian Ultra-Cricket', external_id: 42, active: false }
       let(:sport2) { build :sport, name: 'Brockian Ultra-Cricket', external_id: 43 }
 
       it 'raises no error' do
@@ -41,7 +53,6 @@ RSpec.describe Sport, type: :model do
         expect { sport2.save! }.to_not raise_error
       end
     end
-
   end
 
   context 'calling scope `active`' do
@@ -51,6 +62,34 @@ RSpec.describe Sport, type: :model do
     it 'returns actives only' do
       expect(described_class.active).to eq([sport1])
       expect(described_class.active).to_not eq([sport1, sport2])
+    end
+  end
+
+  context 'inactivating a record' do
+    let(:sport) { create :sport }
+
+    it 'succeeds' do
+      sport.inactivate
+      expect(sport.active).to be(false)
+    end
+  end
+
+  context 'activating a record' do
+    let(:sport) { create :sport, :inactive }
+
+    it 'succeeds' do
+      sport.activate
+      expect(sport.active).to be(true)
+    end
+  end
+
+  context 'reactivating a record' do
+    let!(:sport1) { create :sport, name: 'Brockian Ultra-Cricket', external_id: 42, active: false }
+    let!(:sport2) { create :sport, name: 'Brockian Ultra-Cricket', external_id: 43 }
+
+    it 'inactivates others' do
+      expect { sport1.activate }.to change { sport2.reload.active }.from(true).to(false)
+      expect(sport1.active).to be(true)
     end
   end
 end
